34f6c365428529ad24e0542375cf70f81612f791
arslanbitsclan <arslanbitsclan@gmail.com>
2024-12-09 06:15:24 +0500

    feature-(define a constant and use if statement )

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,9 @@
 //PascalCasing
 function Message() {
   //JSX: Javascript XML
-  return <h1>Hello world</h1>;
+  const name = "Arslan";
+  if (name) return <h1>Hello world</h1>;
+  return <h1>No Result found</h1>;
 }
 
 export default Message;
